0d1633738b42711ccfee8b114c3c8804c047443b
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-01-02 21:31:09 -0700

    Wednesday night group changes to JS/HTML (modals, meetUp API)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,9 +10,6 @@ $(document).ready(function() {
       };
       firebase.initializeApp(config);
 
-    // Create a variable to reference the database
-    const database = firebase.database();
-
     // Capture Button Click
     $("#add-search").on("click", function(event) {
         // Don't refresh the page!
@@ -25,7 +22,7 @@ $(document).ready(function() {
         let jobType = $(`#job-type-input`).val();
         console.log(jobType);
         
-        // Create AJAX call based on user inut
+        // Create AJAX call based on user input
         let queryURL = `https://authenticjobs.com/api/?api_key=25915f6b6bd9671779f4cb0d43be8b66&format=json&method=aj.jobs.search&category=${category}&type=${jobType}&perpage=30`;
 
         $.ajax({
@@ -58,7 +55,7 @@ $(document).ready(function() {
                         location = response.listings.listing[i].company.location.name;
                     }
                     
-            let dataRow = `<tr><td>${response.listings.listing[i].company.name}</td><td>${location}</td><td>${response.listings.listing[i].title}</td><td>${response.listings.listing[i].type.name}</td><td>${tagline}</td><td><button class="bg-info text-white specific-job" id="${response.listings.listing[i].company.location.name}" data-toggle="modal" data-target="#specificModal">Find Out More!</button></td></tr>`;
+            let dataRow = `<tr><td>${response.listings.listing[i].company.name}</td><td>${location}</td><td>${response.listings.listing[i].title}</td><td>${response.listings.listing[i].type.name}</td><td>${tagline}</td><td><button class="bg-info text-white specific-job" id="${response.listings.listing[i].company.location.name}" data-toggle="modal" data-target="#specificModal" lat="${response.listings.listing[i].company.location.lat}" lng="${response.listings.listing[i].company.location.lng}" url="${response.listings.listing[i].apply_url}">Find Out More!</button></td></tr>`;
             
             $(`#job-info`).append(dataRow);
             }
@@ -71,92 +68,70 @@ $(document).ready(function() {
     $("#job-info").on("click", ('.specific-job'), function(event) {
         // Don't refresh the page!
         event.preventDefault();
+        $(`#localMap`).empty();
+        $(`#meetUps`).empty();
 
-        let city = $(this).val(`id`);
+        let city = $(this).attr(`id`);
         console.log(city);
+        let latPointer = $(this).attr('lat');
+        let lngPointer = $(this).attr(`lng`);
+        let applyURL = $(this).attr(`url`);
 
         $(`#specificModal`).modal(`toggle`);
 
-        let image = $(`<img class="map" src='https://maps.googleapis.com/maps/api/staticmap?center=${city}&zoom=10&size=600x300&maptype=roadmap&key=AIzaSyCPpsNM_ZFTCJH9aNrS-mWO4D8t_FHDh4k'>`);
+        let image = $(`<img class="map" src='https://maps.googleapis.com/maps/api/staticmap?center=${city}&markers=color:red%7C${latPointer},${lngPointer}&zoom=11&size=400x150&maptype=roadmap&key=AIzaSyCPpsNM_ZFTCJH9aNrS-mWO4D8t_FHDh4k'>`);
 
         $(`#localMap`).append(image);
+        $(`#specificURL`).attr(`href`, applyURL);
+
+        // Create AJAX call based on user input
+        let meetURL = `https://api.meetup.com/find/groups?location=${city}&radius=0.5&category=34&order=distance&offset=0&page=15&format=json&key=753e255f6c517b7a17724b111556155`;
+
+        $.ajax({
+            url: meetURL,
+            method: `GET`
+
+        }).then(function(response) {
+            console.log(response);
+
+        for(let j = 0; j < response.length; j++) {
+            let groupName = response[j].name;
+            let groupURL = response[j].link;
+            console.log(groupName);
+            console.log(groupURL);
+
+            let nameDiv = $(`<a href='${groupURL}' target='_blank'>${groupName}<br></a>`);
+            $(`#meetUps`).append(nameDiv);
+            
+        }
+
+        });
+
      }); // Closes 'more info' on-click function
 
 
+
+
+     // Capture 'apply later' button click
+     $(document).on("click", ('#apply-later'), function(event) {
+        // Don't refresh the page!
+        event.preventDefault();
+
+        let saveURL = $(`#specificURL`).attr('href');
+        console.log(saveURL);
+
+        // Create a variable to reference the database
+        const database = firebase.database();
+
+        //Provides initial data to Firebase database
+        database.ref().push({
+            URL: saveURL,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+        }); // Closes database.ref.push function
+
+     }); // Closes 'apply later' on-click function
     
-    // Provides initial data to Firebase database
-    // database.ref().push({
-    //     keyword: keyword,
-    //     location: location,
-    //     jobType: jobType,
-    //     company: company,
-    //     dateAdded: firebase.database.ServerValue.TIMESTAMP
-    // }); // Closes database.ref.push function
     
-    // $(`#keyword-input`).val(``);
-    // $(`#location-input`).val(``);
-    // $(`#job-input`).val(``)
-    // $(`#company-input`).val(``)
+    
 
-
-    // displayJobs function re-renders the HTML to display the appropriate content, creates API call
-    // function displayJobs() {
-    //     //$(`#jobs-view`).empty();
-
-    //     //let job = $(this).attr(`data-name`)
-    //     let queryURL = `https://authenticjobs.com/api/?api_key=25915f6b6bd9671779f4cb0d43be8b66&format=json&method=aj.jobs.search&category=4&perpage=30`;
-        
-    //     //`https://data.usajobs.gov/api/search?JobCategoryCode=2200&Keyword=web develop&PositionSchedule=1&WhoMayApply=public&HiringPath=public;graduates&ResultsPerPage=15`
-        
-    //     //`https://api.github.com/?access_token=2d5c338354b50de33dd037057119f477b4d6d73a`
-    //     //let host = `data.usajobs.gov`;
-    //     //let userAgent = `ethantatum81@gmail.com`;
-    //     //let authKey = `h1Z1jDLKTLTCLTiejmf6tA+zXLn1r6tZq9O1nEZZdV4=` 
-
-    //     // Creates AJAX call for the specific toon button being clicked
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: `GET`,
-    //         //headers: {
-    //             //"Host": host,
-    //             //"User-Agent": userAgent,
-    //             //"Authorization-Key": authKey
-    //         //}
-    //     }).then(function(response) {
-    //         console.log(response);
-    //         for(let i = 0; i < response.listings.listing.length; i++) {
-            
-    //             let taglineObj = response.listings.listing[i].company.tagline;
-    //             let tagline;
-    //                 if(taglineObj === undefined) {
-    //                     tagline = `<em>None listed</em>`;
-                         
-    //                 } else {
-    //                     tagline = taglineObj;
-    //                 }
-                    
-    //             let dataRow = `<tr><td>${response.listings.listing[i].company.name}</td><td>${response.listings.listing[i].company.location.name}</td><td>${response.listings.listing[i].title}</td><td>${response.listings.listing[i].type.name}</td><td>${tagline}</td><td><button class="bg-info text-white" id="specific-job">Find Out More!</button></td></tr>`;
-
-    //             $(`#job-info`).append(dataRow);
-    //     }
-            
-    //     }); 
-
-    // } // Closes displayJobs function
-
-    //displayJobs();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+}); // Closes 'document ready' function
